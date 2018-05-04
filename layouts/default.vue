ae88b7dd50e675e79942374ad5d06f131c1e4bfb
Tobias <info@tobias-thiele.de>
2018-05-04 04:16:31 +0200

    Initial upload

---
+++ layouts/default.vue
@@ -0,0 +1,146 @@
+<template>
+    <div>
+
+        <!--<header>-->
+
+            <!--<nav class="navbar navbar-expand-lg navbar-dark brown darken-3">-->
+                <!--<div class="container">-->
+                    <!--<a class="navbar-brand" href="#">-->
+                        <!--<div class="logo">-->
+                            <!--<div class="bold">The lost Egyptian</div>-->
+                            <!--<div class="small">Amun's Throne</div>-->
+                        <!--</div>-->
+                    <!--</a>-->
+                    <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+                        <!--<span class="navbar-toggler-icon"></span>-->
+                    <!--</button>-->
+                    <!--<div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+                        <!--<ul class="navbar-nav mr-auto">-->
+                            <!--<li class="nav-item active">-->
+                                <!--<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
+                            <!--</li>-->
+                            <!--<li class="nav-item">-->
+                                <!--<a class="nav-link" href="#">Link</a>-->
+                            <!--</li>-->
+                            <!--<li class="nav-item">-->
+                                <!--<a class="nav-link" href="#">Profile</a>-->
+                            <!--</li>-->
+                        <!--</ul>-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</nav>-->
+
+            <!--<div class="view intro-2">-->
+                <!--<div class="full-bg-img">-->
+                    <!--<div class="mask rgba-black-strong flex-center">-->
+                        <!--<div class="container">-->
+                            <!--<div class="white-text text-center wow fadeInUp">-->
+                                <!--<h2>This Navbar isn't fixed</h2>-->
+                                <!--<h5>When you scroll down it will disappear</h5>-->
+                                <!--<br>-->
+                                <!--<p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>-->
+                            <!--</div>-->
+                        <!--</div>-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</div>-->
+
+        <!--</header>-->
+        <!--&lt;!&ndash;Main Navigation&ndash;&gt;-->
+
+        <!--Main Layout-->
+        <main>
+            <nuxt/>
+        </main>
+        <!--Main Layout-->
+
+        <!--Footer-->
+        <footer class="page-footer font-small brown-darken-3 pt-0">
+
+            <!--Footer Links-->
+            <div class="container">
+
+                <!--First row-->
+                <div class="row">
+
+                    <!--First column-->
+                    <div class="col-md-12 py-5">
+
+                        <div class="mb-5 flex-center">
+
+                            <h3 class="bolder">GameBreakers</h3>
+
+                        </div>
+                    </div>
+                    <!--/First column-->
+                </div>
+                <!--/First row-->
+            </div>
+            <!--/Footer Links-->
+
+            <!--Copyright-->
+            <div class="footer-copyright py-3 text-center">
+                © 2018 Copyright: GameBreakers,
+                <a href="https://www.bib.de/de/standorte/paderborn.html"> bib International College</a>
+                <a class="github" href="#"><i class="fa fa-github"></i></a>
+            </div>
+            <!--/Copyright-->
+
+        </footer>
+        <!--/Footer-->
+
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/mdb.min.js"></script>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .view {
+        background: url("https://mdbootstrap.com/img/Photos/Others/img (51).jpg")no-repeat center center;
+        background-size: cover;
+    }
+    @media (min-width: 768px) {
+        .view {
+            overflow: visible;
+            margin-top: -56px;
+        }
+    }
+    .navbar {
+        z-index: 1;
+
+        .logo {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            .bold {
+                font-weight: 500;
+            }
+
+            .small {
+                line-height: 10px;
+                font-size: 14px;
+            }
+        }
+    }
+
+    .container {
+        text-align: left;
+    }
+
+    footer {
+        position: relative;
+
+        .bolder {
+            font-weight: 700;
+        }
+
+        .github {
+            position: absolute;
+            bottom: 15px;
+            right: 15px;
+        }
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,185 @@
+<template>
+    <div>
+        <div class="header-container">
+            <div class="container h-100">
+                <h1>The lost Egyptian</h1>
+                <h2>Amun's Throne</h2>
+            </div>
+            <div class="char">
+                <img src="../assets/images/character/char.png">
+            </div>
+            <div class="boss">
+                <img src="../assets/images/character/boss.png">
+            </div>
+        </div>
+
+        <img src="../assets/images/balken.png" class="divider-top" alt="">
+        <div class="main-outer">
+            <div class="container py-4">
+                <div class="row">
+                    <h3 class="text-center w-100 container-headline mb-4">Story</h3>
+                    <p>
+                        Im Jahre 1620 vor Christi wurde Amun im alten Ägypten geboren. Ganze 2 Jahre nach ihm kam sein Bruder Bata auf die Welt. Amun war stets der Liebling des aktuell herrschenden Pharaos, da er der erstgeborene Sohn war. Deshalb musste Bata stets um die Aufmerksamkeit kämpfen, dennoch spielten beide gerne zusammen im Nil. Eines Tages, während die beiden Brüder im Nil spielten, entdeckte Bata ein altes Artefakt, welches einer Kugel (mit Krokodilskopf als Gravur darauf) glich und versteckte es über die Jahre. Bata war bewusst, dass es etwas mit Sobek zu tun haben musste. Viele Jahre vergingen.
+                    </p>
+                    <p>
+                        Amun musste im Alter von 17 Jahren den Thron übernehmen und erinnerte sich stets an die Worte seines Vaters "Halte unsere Traditionen aufrecht". Er herrschte Weise und Gerecht, deshalb war er sehr beliebt beim Volk. Für Bata war dies aber Dorn im Auge, denn er wollte gern der neue Pharao sein.
+                    </p>
+                    <p>
+                        Nach bereits 7 Jahren der Regentschaft von Amun, der als Pharao den Namen Amun-Ra erhielt, trat eine lange Trockenzeit in Ägypten ein und das Volk fing an zu Hungern. Um das Wasser wieder etwas steigen zu lassen und damit die Felder wieder zu bewässern, wäre ein menschliches Opfer gegenüber Sobek von Nöten gewesen. Amun-Ra war nicht gewillt ein Menschenopfer zu bringen, da er das Leben als wertvoll erachtete. Bata war in dieser Zeit immer nur der Kommandant über das Heer gewesen und hatte somit seinen Platz in der Gesellschaft eingenommen. Die Not ließ ihn aber an das Artefakt erinnern, kurz darauf benutzte er es in einem Ritual, welches Sobek erscheinen ließ. Sobek ging mit Bata einen Pakt ein, welcher lautete "geb mir Amun-Ra als Opfer, dafür erhälst du den Thron und das Volk wird dich ehren." Bata ging diesen Pakt natürlich ein. Eines Nachts schlich er sich in das Schlafgemach seines Bruders, knebelte diesen und warf in in den Nil, worauf hin Amun-Ra darin ertrunk. Sobek sah damit den Pakt als erfüllt, ließ das Wasser nach Ägypten zurückkehren und gab Bata den Thron. Da das Wasser zurückkehrte, war das Volk wieder in der Lage die Felder zu bewässern und erlangte die ursprüngliche Zufriedenheit fürs erste zurück. Bata, der nun als Pharao Bata-Ra bekannt war, erwies sich jedoch als schlechter Regent. Er ließ Menschen wegen kleinen Fehlern an Bauten hinrichten oder ließ sie in einen Teich von Scorpionen fallen.
+                    </p>
+                    <p>
+                        Das Volk war immer unzufriedener mit dem neuen Pharao und betete immer wieder zu der Göttin Ammit in der Hoffnung dort erhört zu werden. Ammit war mit der Opfergabe von Amun-Ra nicht zufrieden, denn normal sollte sie nur die Herzen fressen, welche unrein waren. Amun-Ra war aber unrechtmäßig im Totenreich gelandet, er hatte ein zu reines Herz, weshalb sie das Flehen des Volkes erhörte und Amun-Ra zurück in die Menschenwelt schickte, um dem Volk auf ein Neues zu dienen.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <img src="../assets/images/balken.png" class="divider-bottom" alt="">
+
+        <img src="../assets/images/balken.png" class="divider-top" alt="">
+        <div class="main-outer">
+            <div class="container py-4">
+                <div class="row">
+                    <h3 class="text-center w-100 container-headline mb-4">Hauptcharaktere</h3>
+                    <div class="col-4">
+                        <div class="char text-center">
+                            <div class="image">
+                                <img src="../assets/images/character/char.png">
+                            </div>
+                            <h3 class="container-headline mb-0">Amun</h3><h6 class="w-100 text-center mb-4">(Amun-Ra)</h6>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <h5 style="margin-top: 70px;" class="char-description container-headline mb-2">Hauptchar<span class="small ml-2 mb-4">(Spielbarer Character)</span></h5>
+                        <p>
+                            Amun-Ra ist der älteste Sohn von Vata-Ra und wurde 1620 vor Christi im alten Ägypten geboren. Er übernimmt den Thron nachdem sein Vater verstorben war und galt als ein gerechter und weiser Pharao. Er selbst hatte eigentlich immer ein gutes Verhältnis zu seinem Bruder Bata gehalten. Jedoch wurde Amun von Bata an Sobek geopfert, um die Trockenzeit zu beenden. Er selbst hatte sein Schicksal schon akzeptiert, aber Ammit, die Jenseitsgöttin, weigerte sich Amun im Totenreich aufzunehmen, da er unrechtmäßig dort gelandet war. Diese sandte ihn zurück und nun versucht er seinen Thron wieder zurück zu erlangen.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container py-4 mt-5">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="char text-center">
+                            <div class="image">
+                                <img style="margin-top: -30px;" src="../assets/images/character/boss.png">
+                            </div>
+                            <h3 class="container-headline mb-0">Sobek</h3>
+                        </div>
+                    </div>
+                    <div class="col-8">
+                        <h5 style="margin-top: 110px;" class="char-description container-headline mb-2">Endboss</h5>
+                        <p>
+                            Sobek ist der Gott des Wassers, der Fruchtbarkeit und Beschützer der Pharaonen. Sobek verlangte ein Menschenleben als Opfer, damit er der Trockenzeit in Ägypten beendete. Amun-Ra weigerte sich dieses Opfer zu bringen, weshalb Bata mit Sobek einen Pakt schloss. „Gib mir Amun-Ra als Opfer, dafür erhältst du den Thron und das Volk wird dich ehren.“, das war der Pakt zwischen den beiden. Nachdem der Pakt erfüllt war, ließ Sobek das Wasser nach Ägypten zurückkehren und erfüllte damit seinen Part. Sobek wacht mit einigen anderen Göttern seitdem über das Grab von Amun-Ra.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="../assets/images/balken.png" class="divider-bottom" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+        },
+        data() {
+            return {
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @font-face {
+        font-family: Egyptian;
+        src: url('~/assets/fonts/Egyptian_Nights.ttf');
+    }
+
+    body {
+        background-color: #4e342e;
+    }
+
+    .header-container {
+        position: relative;
+        background: rgba(206,170,126,1);
+        background: linear-gradient(to bottom, rgba(206,170,126,1) 0%, rgba(249,224,179,1) 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ceaa7e', endColorstr='#f9e0b3', GradientType=0 );
+        height: 50vh;
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+
+            h1 {
+                font-family: Egyptian, 'Arial Black', sans-serif;
+                font-size: 100px;
+                text-align: center;
+            }
+
+            h2 {
+                font-size: 46px;
+                font-weight: 300;
+            }
+        }
+
+        .char {
+            position: absolute;
+            bottom: 4em;
+            left: 10%;
+
+            img {
+                height: 300px;
+                filter: drop-shadow(5px 5px 5px #222);
+            }
+        }
+
+        .boss {
+            position: absolute;
+            bottom: 3em;
+            right: 10%;
+
+            img {
+                height: 300px;
+                filter: drop-shadow(5px 5px 5px #222);
+            }
+        }
+    }
+
+    img.divider-top {
+        width: 100%;
+        &:first-of-type {
+            margin: 5px 0 0 0;
+        }
+    }
+
+    img.divider-bottom {
+        width: 100%;
+        margin: 0 0 5px 0;
+        transform: rotate(180deg);
+    }
+
+    .main-outer {
+        background: rgba(206,170,126,1);
+        background: linear-gradient(to top, rgba(206,170,126,1) 0%, rgba(249,224,179,1) 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ceaa7e', endColorstr='#f9e0b3', GradientType=0 );
+
+        .container-headline {
+            font-weight: 700;
+            text-transform: uppercase;
+        }
+
+        .char {
+            .image {
+                img {
+                    height: 350px;
+                    margin-top: -100px;
+                }
+            }
+        }
+    }
+
+</style>
